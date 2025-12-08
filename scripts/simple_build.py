5d6bc234a3d0a48c506dd4578bc5bef99564e565
mexi <davidiego2@dyd2.home>
2025-12-07 19:28:41 -0600

    Increase logo size to 60px (1.5x)

--- scripts/simple_build.py
+++ scripts/simple_build.py
@@ -146,7 +146,7 @@ def generate():
     <nav class="navbar">
         <div class="container">
             <a href="../index.html" class="logo">
-                <img src="../static/images/banda_dark.png" alt="BandaWeb3" style="height: 40px;">
+                <img src="../static/images/banda_dark.png" alt="BandaWeb3" style="height: 60px;">
             </a>
             <div class="nav-links">
                 <a href="../numbered.html">Episodios Numerados</a>
